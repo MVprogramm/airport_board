bac34a7d2b2d459ad3113bef3f7a7d7d557dc09a
michael valiahmetov <michael.valiahmetov@gmail.com>
2022-08-07 11:16:22 +0300

    fix board menu

--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,21 +1,21 @@
-import React from 'react';
-import './menu.scss';
+import React from "react";
+import "./menu.scss";
 
 const Menu = () => (
   <div className="board__menu">
-    <div className="menu__decor menu__decor_left menu__bground_white menu__decor_show"></div>
+    <div className="menu__decor menu__decor_left menu__bground_white"></div>
     <div className="menu__field menu__field_dep menu__bground_white">
       <div className="menu__icon menu__icon_dep menu__icon_blue menu__bground_white"></div>
       <div className="menu__title menu__title_blue">Departures</div>
     </div>
-    <div className="menu__decor menu__decor_center menu__bground_blue menu__decor_show">
+    <div className="menu__decor menu__decor_center menu__bground_blue">
       <div className="menu__decor menu__decor_right menu__bground_white menu__decor_cross"></div>
     </div>
     <div className="menu__field menu__field_arr menu__bground_blue">
       <div className="menu__icon menu__icon_arr menu__icon_white menu__bground_blue"></div>
       <div className="menu__title menu__title_white">Arrivals</div>
     </div>
-    <div className="menu__decor menu__decor_right menu__bground_blue menu__decor_show"></div>
+    <div className="menu__decor menu__decor_right menu__bground_blue"></div>
   </div>
 );
 
